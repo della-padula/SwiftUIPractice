5c6b29f0e33bef086499fe8ff4ba6629cf48da64
denny.k <denny.k@kakaocorp.com>
2020-03-27 23:31:17 +0900

    VStack, HStack, CustomHStack

--- SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/ContentView.swift
@@ -2,15 +2,38 @@
 //  ContentView.swift
 //  SwiftUIPractice
 //
-//  Created by 김태인 on 2020/03/27.
-//  Copyright © 2020 김태인. All rights reserved.
+//  Created by denny.k on 2020/03/27.
+//  Copyright © 2020 denny.k. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        let myString: String = "Welcome to Swift UI"
+        
+        return VStack {
+            VStack {
+                Text("Text 1")
+                Text("Text 2")
+                MyHStackView()
+                HStack {
+                    Text("Text 3")
+                    Text("Text 4")
+                }
+                Text("Hello, ") + Text("How ") + Text("are you?")
+            }
+            Text("\(myString)")
+        }
+    }
+}
+
+struct MyHStackView: View {
+    var body: some View {
+        HStack {
+            Text("Text 5")
+            Text("Text 6")
+        }
     }
 }
 
