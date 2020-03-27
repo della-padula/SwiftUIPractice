e4819e2d50a33163d6b211df932dfea7bd713364
denny.k <denny.k@kakaocorp.com>
2020-03-27 23:20:38 +0900

    Practice Initialize

---
+++ SwiftUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by 김태인 on 2020/03/27.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
